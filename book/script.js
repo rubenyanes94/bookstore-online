33ca99b10a63f8bef0ee132992e7136a15bbac97
Rubeny94 <44436716+rubenyanes94@users.noreply.github.com>
2024-09-04 12:40:21 -0400

    Update script.js

--- book/script.js
+++ book/script.js
@@ -2,7 +2,7 @@ const btnVerMas = document.querySelector('.btn-ver');
 const libros = document.querySelectorAll('.book-item');
 const librosPorPagina = 3; 
 
-
+//Catalogo
 for (let i = librosPorPagina; i < libros.length; i++) {
     libros[i].classList.add('oculto');
 }
@@ -15,4 +15,22 @@ btnVerMas.addEventListener('click', () => {
     } else {
         btnVerMas.style.display = 'none';
     }
+});
+
+//Menu 
+const navLinks = document.querySelectorAll('nav a[href^="#"]');
+
+navLinks.forEach(link => {
+  link.addEventListener('click', (event) => {
+    event.preventDefault(); 
+
+    const targetId = link.getAttribute('href').substring(1);
+
+    const element = document.getElementById(targetId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    } else {
+      console.error(`El elemento con ID "${targetId}" no existe.`);
+    }
+  });
 });
